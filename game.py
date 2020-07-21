a443ce3f39ed4be4ed72d222a8abd3a48e9f6440
Jonathan Finger <jaefinger@gmail.com>
2020-07-21 13:51:07 -0500

    Fixed issue #1

--- game.py
+++ game.py
@@ -1,17 +1,25 @@
 import random
 
-numb = random.randint(1, 100)
+# numb = random.randint(1, 100)
+numb = 50
 guess = -1
 
 def main():
-    print("hello and welcome to my game")
-    guess = input("Please select a number between 1 and 100")
-    # if int(numb) <= 100:
-    #     print("good job!")
-    # else:
-    #     print("You fail!")
-    print(f"You guessed {guess}\n")
-    print(f"The computer's actual choice was: {numb}")
+    print("Hello and welcome to my test game!")
+    guess = input("Please select a number between 1 and 100\n")
+    guess = int(guess)
+    if 100 >= guess >= 1:
+        if guess > numb:
+            print(f"Your guess is too high, the computer guessed {numb}")
+        elif guess < numb:
+            print(f"Your guess is too low, the computer guessed {numb}")
+        elif guess == numb:
+            print(f"You guessed {guess} and the computer had the number {numb} selected. Great job!")
+        else:
+            print("You selected a number but something went wrong :(")
+    else:
+        print("You didn't select a number :(")
+
 
 if __name__ == '__main__':
     main()
