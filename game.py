3baa8dd507466b6c427dcbaeb0bc636b03ad9434
Jonathan Finger <jaefinger@gmail.com>
2020-07-21 13:30:13 -0500

    updates game

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import random
 
+numb = random.randint(1, 100)
+guess = -1
+
 def main():
     print("hello and welcome to my game")
-    numb = input("Please select a number between 1 and 100")
-    if int(numb) <= 100:
-        print("good job!")
-    else:
-        print("You fail!")
+    guess = input("Please select a number between 1 and 100")
+    # if int(numb) <= 100:
+    #     print("good job!")
+    # else:
+    #     print("You fail!")
+    print(f"You guessed {guess}\n")
+    print(f"The computer's actual choice was: {numb}")
 
 if __name__ == '__main__':
     main()
